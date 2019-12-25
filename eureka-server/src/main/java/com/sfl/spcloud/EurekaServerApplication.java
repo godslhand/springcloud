2587bebb56c7aee11fff7f4b239ee2e35e2f5513
godslhand <137734810@qq.com>
2019-12-25 17:06:25 +0800

    eureka

---
+++ eureka-server/src/main/java/com/sfl/spcloud/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.sfl.spcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author songfenglai
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
